c16566f78b88d409333ccd6eb68c9c7a23bed294
Tushar Walavalkar <Tushar.W@WinWire.com>
2025-03-06 13:27:53 -0600

    Implemented CORS.

--- FHIR/Program.cs
+++ FHIR/Program.cs
@@ -10,17 +10,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(c => c.AllowAnyHeader()
+									.AllowAnyMethod()
+									.WithOrigins("https://localhost:4200", "http://localhost:4200")
+);
 
 app.UseAuthorization();
 
